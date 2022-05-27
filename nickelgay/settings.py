a8e6aab2439eb55e08d9c0ffc540aa4132226c48
jotte <71946106+jottew@users.noreply.github.com>
2022-05-27 15:41:23 +0200

    Partially added i18n and made the nav menu automatically generate

--- nickelgay/settings.py
+++ nickelgay/settings.py
@@ -105,13 +105,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+USE_L10N = True
 
 USE_TZ = True
+
+LANGUAGES = [
+    ('de','German'),
+    ('en', 'English')
+]
 
 
 # Static files (CSS, JavaScript, Images)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
+import json
+
 from django.http import *
 from django.shortcuts import render, redirect
 from .api import valid_filetypes
 from django.contrib.auth.decorators import login_required
+from django.utils.translation import gettext
 
 platforms = {
     "YouTube": "YouTube",
@@ -52,45 +55,130 @@ valid_audio_types = [
     "aac"
 ]
 
+"""
+authenticated:
+    True: specifically is authenticated
+    False: specifically is not authenticated
+    None: doesn't matter
+"""
+nav = {
+    "../": {
+        "admin": False,
+        "authenticated": None,
+        "name": gettext("Home")
+    },
+    "/download/": {
+        "admin": False,
+        "authenticated": None,
+        "name": gettext("Download")
+    },
+    "/shazam/": {
+        "admin": False,
+        "authenticated": None,
+        "name": gettext("Shazam")
+    },
+    "/effects/": {
+        "admin": False,
+        "authenticated": None,
+        "name": gettext("Effects")
+    },
+    "/admin/": {
+        "admin": True,
+        "authenticated": True,
+        "name": gettext("Admin")
+    },
+    "/login/": {
+        "admin": False,
+        "authenticated": False,
+        "name": gettext("Login")
+    },
+    "/logout/": {
+        "admin": False,
+        "authenticated": True,
+        "name": gettext("Logout")
+    },
+}
+
+
+def create_nav(request: HttpRequest):
+    new_nav = nav.copy()
+
+    admin = [k for k, v in new_nav.items() if v["admin"]]
+    authenticated = [k for k, v in new_nav.items() if v["authenticated"] is True]
+    not_authenticated = [k for k, v in new_nav.items() if v["authenticated"] is False]
+
+    if not request.user.is_staff:
+        for i in admin:
+            try:
+                new_nav.pop(i)
+            except Exception:
+                pass
+
+    if not request.user.is_authenticated:
+        for i in authenticated:
+            try:
+                new_nav.pop(i)
+            except Exception:
+                pass
+
+    if request.user.is_authenticated:
+        for i in not_authenticated:
+            try:
+                new_nav.pop(i)
+            except Exception:
+                pass
+
+    navs = dict([(v["name"], k) for k, v in new_nav.items()])
+
+    return navs.items()
+
 
 def home_view(request: HttpRequest):
+    new_nav = create_nav(request)
+
     return render(
         request,
         "home.html",
+        context={
+            "nav": new_nav
+        }
     )
 
 
 @login_required
 def download_view(request: HttpRequest):
+    new_nav = create_nav(request)
+
     return render(
         request,
         "download.html",
         context={
             "platforms": platforms.items(),
             "types": types,
-            "resolutions": resolutions
+            "resolutions": resolutions,
+            "nav": new_nav
         }
     )
 
 
 @login_required
 def shazam_view(request: HttpRequest):
+    new_nav = create_nav(request)
     valid_types = ", ".join([i.upper() for i in valid_filetypes[:-1]])+f" or {valid_filetypes[-1].upper()} (MAX. 8mb)."
 
     return render(
         request,
         "shazam.html",
         context={
-            "valid_types": valid_types
+            "valid_types": valid_types,
+            "nav": new_nav
         }
     )
 
 
 @login_required
 def effects_view(request: HttpRequest):
-    if not request.user.is_authenticated:
-        return redirect("/login")
-
+    new_nav = create_nav(request)
     valid_types = valid_video_types+valid_audio_types
     valid_types = ", ".join([i.upper() for i in valid_types[:-1]])+f" or {valid_types[-1].upper()} (MAX. 8mb)."
 
@@ -101,6 +189,7 @@ def effects_view(request: HttpRequest):
             "valid_types": valid_types,
             "effects": effects.items(),
             "audio_types": ",".join(valid_audio_types),
-            "video_types": ",".join(valid_video_types)
+            "video_types": ",".join(valid_video_types),
+            "nav": new_nav
         }
     )
